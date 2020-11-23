7849da68a7f519cac8dca40e1fd0da1fbc266e19
1004gite <1004gite@naver.com>
2020-11-23 21:15:36 +0900

    .

--- routes/room_state.js
+++ routes/room_state.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('./db');
 
 router.get('/', function(req, res, next) {
     res.render('../views/chanwoong/room_state', { title: 'Room_state' , cust_info:null});
