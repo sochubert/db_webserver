8b23ae39c95fd71ed4cedb8f1ba4438d28b241fb
1004gite <1004gite@naver.com>
2020-11-26 21:39:00 +0900

    stop

--- public/js/roomstate.js
+++ public/js/roomstate.js
@@ -1,26 +1,17 @@
- $(function (){
-    var socket = io();
 
+$(function (){
+    var socket = io();
     socket.on("changestate", function (data){
         var id = data.roomnum;
         //아이디로 접근해서 색 바꾸기
         $("#"+id);
+        console.log("=-=-=-=-=\n\n\n\n");
     });
 
 
-    //클릭 됐을 때
-     for(var j = 10;j > 0;j--) {
-        for(var i = 1; i < 13; i++) {
-            var id =100*j+i;
-            id = toString(id);
-            $("#"+id).submit(function (e){
-                e.preventDefault();
-                socket.emit("changeroomstate",{
-                    roomnum:id,
-                    state:null,
-                });
-            });
-        };
-     };
-
  });
+
+function myclick(){
+    console.log("asdasdasd");
+    console.log(x);
+}
