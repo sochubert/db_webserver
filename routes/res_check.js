cb7590d862458e381aa64a1eab3671e1b8740d21
Taeyoon_Kwon <kwonconnor101@gmail.com>
2020-12-10 15:44:35 +0900

    발표전 마지막 수정

--- routes/res_check.js
+++ routes/res_check.js
@@ -11,9 +11,13 @@ router.get('/', function(req, res, next) {
     if(x == false) {//회원
         sql = "SELECT * FROM RESERVATION R LEFT JOIN CUSTOMER C ON R.CUSTOMER_ID = C.CUSTOMER_ID LEFT JOIN PERSON P ON P.PERSON_ID = C.PERSON_ID WHERE C.CUSTOMER_ID = " + req.cookies['customer_id'].toString();
         connection.query(sql, function (error, result) {
+            if(error){
+                console.log(error);
+            }
+            console.log(result);
             res.render('chanwoong/res_check', {
                 title: 'Reservation_Check',
-                cust_info: [req.cookies.employee_id, req.cookies.employee_name, x],
+                cust_info: [req.cookies.customer_id, req.cookies.customer_name, x],
                 res_check:result,
             });
         });
@@ -54,33 +58,6 @@ router.post('/', function(req, res, next) {
         }
     });
 });
-
-
-
-// router.post('/rm', function(req, res, next) {
-//     var x;
-//     if(!req.cookies['customer_id'] && !req.cookies['employee_id']) x = null;
-//     else if(!req.cookies['employee_id']) x = false;
-//     var res1 = req.body.res1;//resid
-//     var sql;
-//     sql =  "SELECT * FROM RESERVATION R LEFT JOIN CUSTOMER C ON R.CUSTOMER_ID = C.CUSTOMER_ID LEFT JOIN PERSON P ON P.PERSON_ID = C.PERSON_ID WHERE R.RESERVATION_ID =" + res1.toString();
-//     connection.query(sql, function (error, resinfo) {
-//         var phone = resinfo[0].PHONE_NUM.toString();
-//
-//         var sql2 = "SELECT * FROM RESERVATION R LEFT JOIN CUSTOMER C ON R.CUSTOMER_ID = C.CUSTOMER_ID LEFT JOIN PERSON P ON P.PERSON_ID = C.PERSON_ID WHERE P.PHONE_NUM = " + "\""+phone.toString()+ "\"";
-//         connection.query(sql2, function (error, result) {
-//             sql = "DELETE FROM RESERVATION WHERE RESERVATION_ID = ?";
-//             connection.query(sql, [res1.toString()], function (error, no) {
-//                 console.log(result);
-//                 res.render('chanwoong/res_check', {
-//                     title: 'Reservation_Check',
-//                     cust_info: null,
-//                     res_check: null,
-//                 });
-//             });
-//         });
-//     });
-// });
 
 
 
